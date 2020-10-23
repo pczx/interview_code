d6e001e537e747097205403480490a922b5f6dea
chenzhengpeng <chenzhengpeng@vipkid.com.cn>
2020-10-23 14:19:51 +0800

    solution 13、14

--- src/main/java/leetcode/Solution13.java
+++ src/main/java/leetcode/Solution13.java
@@ -2,24 +2,27 @@ package leetcode;
 
 class Solution13 {
     public int movingCount(int m, int n, int k) {
+        if (k < 0 || m <= 0 || n <= 0) {
+            return 0;
+        }
         boolean[] visited = new boolean[m * n];
-        int count = movingCountCore(m, n, k, 0, 0, visited);
+        int count = movingCountCore(0, 0, m, n, k, visited);
         return count;
     }
 
-    private int movingCountCore(int rows, int cols, int k, int row, int col, boolean[] visited) {
+    private int movingCountCore(int row, int col, int rows, int cols, int k, boolean[] visited) {
         int count = 0;
-        if (check(rows, cols, k, row, col, visited)) {
+        if (check(row, col, rows, cols, k, visited)) {
             visited[row * cols + col] = true;
-            count = 1 + movingCountCore(rows, cols, k, row, col - 1, visited)
-                    + movingCountCore(rows, cols, k, row, col + 1, visited)
-                    + movingCountCore(rows, cols, k, row - 1, col, visited)
-                    + movingCountCore(rows, cols, k, row + 1, col + 1, visited);
+            count = 1 + movingCountCore(row, col - 1, rows, cols, k, visited)
+                    + movingCountCore(row, col + 1, rows, cols, k, visited)
+                    + movingCountCore(row - 1, col, rows, cols, k, visited)
+                    + movingCountCore(row + 1, col, rows, cols, k, visited);
         }
         return count;
     }
 
-    private boolean check(int rows, int cols, int k, int row, int col, boolean[] visited) {
+    private boolean check(int row, int col, int rows, int cols, int k, boolean[] visited) {
         if (row >= 0 && row < rows && col >= 0 && col < cols && getDigitSum(row) + getDigitSum(col) <= k && !visited[row * cols + col]) {
             return true;
         }

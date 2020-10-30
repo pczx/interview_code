3d9ab4d2fa139fa39951cf1b3702791b53d4924e
chenzhengpeng <chenzhengpeng@vipkid.com.cn>
2020-10-30 12:40:32 +0800

    剑指offer

---
+++ src/main/java/leetcode/Solution45.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.util.Arrays;
+
+class Solution45 {
+    public String minNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strs, (x, y) -> (x + y).compareTo(y + x));
+        StringBuilder sb = new StringBuilder();
+    }
+}
